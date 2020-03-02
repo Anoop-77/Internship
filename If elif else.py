cadef669767a8118bc8c3f232a912089bf2db581
darkera7 <52588403+darkera7@users.noreply.github.com>
2020-03-02 12:07:34 +0530

    Add files via upload

---
+++ If elif else.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+tired=False
+if tired:
+    print("rest")
+else:
+    print("work")
+
+
+# In[16]:
+
+
+loc = 'store'
+if loc=='temple':
+    print("pray")
+elif loc=='park':
+    print("play")
+elif loc=="store":
+    print("shop")
+else:
+    print("go back home")
+
+
+# In[ ]:
+
+
+
+
